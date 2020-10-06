57c04cef6a9bb47926136cf44aff2dc841e72889
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2020-10-06 10:01:01 +0200

    Add files via upload

---
+++ SQLManager_files/SQLManager.sql
@@ -0,0 +1,10 @@
+poule[1] = new Array("undefined","undefined","undefined","undefined")
+poule[2] = new Array("undefined","undefined","undefined","undefined")
+poule[3] = new Array("undefined","undefined","undefined","undefined")
+poule[4] = new Array("undefined","undefined","undefined","undefined")
+poule[5] = new Array("undefined","undefined","undefined","undefined")
+
+//user
+
+poule[1] = new Array("ùmm","undefined","undefined","Indéterminé")
+poule[1] = new Array("salut","undefined","undefined","Indéterminé")
